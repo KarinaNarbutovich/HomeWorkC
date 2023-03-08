84f559c6635a40ca6b6e46069c6cb2a644ec4574
Karina <setting6996@gmail.com>
2023-03-08 21:23:09 +0300

    done Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int max(int a, int b, int c)
+{
+int result= a;
+if (b>a)
+{
+    result=b;
+}
+if (c>b)
+{
+    result=c;
+}
+return result;
+}
+
+
+Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c=Convert.ToInt32(Console.ReadLine());
+
+int Max=max(a,b,c);
+Console.WriteLine($"Максимальное из чисел ({a}, {b}, {c}) = {Max}");
