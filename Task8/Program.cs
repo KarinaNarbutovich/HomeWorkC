03ce990bdfa4dd34f9a2ab087a6c2c374045a632
Karina <setting6996@gmail.com>
2023-03-08 22:04:09 +0300

    done Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n>=2)
+{
+for (int i=1; i<=n; i++)
+{
+    if (i%2==0)
+    {
+Console.Write($" {i}");
+    }
+}
+}
+else 
+{
+    Console.WriteLine("Введите число больше 1 ");
+}
