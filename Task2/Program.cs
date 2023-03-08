3db2c465951516bd5d9280d429ab43e1c8d97e1a
Karina <setting6996@gmail.com>
2023-03-08 20:43:54 +0300

    done Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine($"Большее число: {a}\n Меньшее число: {b}");
+}
+else if (b>a)
+{
+     Console.WriteLine($"Большее число: {b}\nМеньшее число: {a}");
+}
+else
+{
+    Console.WriteLine($"Числа  равны");
+}
