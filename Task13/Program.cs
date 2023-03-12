8c1ada0ba09218f96536546fb0cc21611c5685e7
Karina <setting6996@gmail.com>
2023-03-12 12:23:48 +0300

    done Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n/100!=0 && n>0)
+{
+    while (n>999)
+    {
+n=n/10;
+    }
+    Console.WriteLine($"Третья цифра числа: {n%10}");
+    }
+else if (n/100!=0 && n<0)
+{
+     while (n<-999)
+    {
+n=n/10;
+    }
+    Console.WriteLine($"Третья цифра числа: {n%10*(-1)}");
+}
+else 
+{
+Console.WriteLine("У данного числа нет третьей цифры");
+}
