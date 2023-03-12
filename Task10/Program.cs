7e784558dafaea8fa2537e5c1230aa3a31ddeffd
Karina <setting6996@gmail.com>
2023-03-12 10:20:28 +0300

    added negative numbers

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,8 +1,12 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int n=Convert.ToInt32(Console.ReadLine());
-if (100<n && n<1000)
+if (99<n && n<1000)
 {
     Console.WriteLine($"Вторая цифра данного числа: {(n/10)%10}");
+}
+else if (-999<n && n<-99)
+{
+Console.WriteLine($"Вторая цифра данного числа: {(n/10)%10*(-1)}");
 }
 else
 {
