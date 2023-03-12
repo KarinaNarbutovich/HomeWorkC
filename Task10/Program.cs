a759be1cb46e0fd46e68aedfeb9a8ade350c0905
Karina <setting6996@gmail.com>
2023-03-12 10:03:46 +0300

    add program file

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+if (100<n && n<1000)
+{
+    Console.WriteLine($"Вторая цифра данного числа: {(n/10)%10}");
+}
+else
+{
+Console.WriteLine("Введите другое число");
+}
