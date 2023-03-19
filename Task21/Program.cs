e1f5bd0a9070edbae38c1f58f57bff3b95354820
Karina <setting6996@gmail.com>
2023-03-19 13:52:28 +0300

    Исправила ошибку

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -12,15 +12,18 @@ Console.WriteLine("Введите координату Z1: ");
 double z1=Convert.ToDouble(Console.ReadLine());
 while (x1==0)
 {
- Console.WriteLine("Вы ошиблись!\nВведите координату X1:  ");  
+ Console.WriteLine("Вы ошиблись!\nВведите координату X1:  "); 
+ x1= Convert.ToDouble(Console.ReadLine()); 
 }
 while (y1==0)
 {
- Console.WriteLine("Вы ошиблись!\nВведите координату Y1:  ");  
+ Console.WriteLine("Вы ошиблись!\nВведите координату Y1:  ");
+ y1= Convert.ToDouble(Console.ReadLine());   
 }
 while (z1==0)
 {
- Console.WriteLine("Вы ошиблись!\nВведите координату Z1:  ");  
+ Console.WriteLine("Вы ошиблись!\nВведите координату Z1:  ");
+ z1= Convert.ToDouble(Console.ReadLine());   
 }
 Console.WriteLine("Введите координату Х2: ");
 double x2=Convert.ToDouble(Console.ReadLine());
@@ -30,14 +33,18 @@ Console.WriteLine("Введите координату Z2: ");
 double z2=Convert.ToDouble(Console.ReadLine());
 while (x2==0)
 {
- Console.WriteLine("Вы ошиблись!\nВведите координату X2:  ");  
+ Console.WriteLine("Вы ошиблись!\nВведите координату X2:  ");
+ x2= Convert.ToDouble(Console.ReadLine());   
 }
 while (y2==0)
 {
- Console.WriteLine("Вы ошиблись!\nВведите координату Y2:  ");  
+ Console.WriteLine("Вы ошиблись!\nВведите координату Y2:  ");
+ y2= Convert.ToDouble(Console.ReadLine());   
 }
+
 while (z2==0)
 {
  Console.WriteLine("Вы ошиблись!\nВведите координату Z2:  ");  
+ z2= Convert.ToDouble(Console.ReadLine()); 
 }
 Console.WriteLine($"Расстояние между данными точками равно: {Math.Round(Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2)),2)} ");
